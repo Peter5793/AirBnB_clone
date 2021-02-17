05e91f4fa2e605c882f068c7c4436f4769fdf8b3
Peter5793 <masaba.peter68@gmail.com>
2021-02-17 09:24:03 +0300

    fix public instance attribute

--- models/base_model.py
+++ models/base_model.py
@@ -2,24 +2,24 @@
 """Base class """
 from uuid import uuid4
 from datetime import datetime
-import models
-
+#import models
 
 class BaseModel:
-    def __init__(self, *args, **kwargs):
-        """constructors"""
-        self.id = str(uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    """Public instance attributes intialization"""
+    id = str(uuid4())
+    created_at = datetime.now()
+    updated_at = datetime.now()
 
     def __str__(self):
         return ("[{}] ({}) {}".format(self.__class__.__name__, self.id,
                 self.__dict__))
+
     def save(self):
         """
         updates the public instance attribute with current datetime
         """
         self.updated_at = datetime.now()
+
     def to_dict(self):
         """
         returns a dictioanry containing all  keys/valu of __dict__
@@ -31,5 +31,5 @@ class BaseModel:
                 dict_str[key] = value.strftime("%Y-%m-%dT%H:%M:%S.%f")
             else:
                 dict_str[key] = value
-        dict_str["__class__"] = self.__class__.name__
-        return dict_str                                                                                                                                                                
+        dict_str["__class__"] = self.__class__.__name__
+        return dict_str
